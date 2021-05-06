8fdd1ed35b855e42397d61ee805ddf317a4aabd2
Dhyon Keyllo <dhyon.kpm@hotmail.com>
2021-05-05 23:26:26 -0300

    lib php mailer add

--- processa_envio.php
+++ processa_envio.php
@@ -1,3 +1,47 @@
 <?php
 
-    print_r($_POST);
+    require "./libs/PHPMailer/Exception.php";
+    require "./libs/PHPMailer/OAuth.php";
+    require "./libs/PHPMailer/PHPMailer.php";
+    require "./libs/PHPMailer/POP3.php"; // CONTEM AS ESPECIFICACOES DO PROTOCOLO DE RECEBIMENTO DE EMAIL
+    require "./libs/PHPMailer/SMTP.php"; // CONTEM O PROTOCOLO DE ENVIO DE EMAIL
+
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+
+    //print_r($_POST);
+
+    class Mensagem {
+        private $para = null;
+        private $assunto = null;
+        private $mensagem = null;
+
+        public function __get($atributo) {
+            return $this->$atributo;
+        }
+
+        public function __set($atributo, $valor) {
+            $this->$atributo = $valor;
+        }
+
+        public function mensagemValida() {
+            if(empty($this->para) || empty($this->assunto) || empty($this->mensagem)) {
+                return false;
+            }
+            return true;
+        }
+    }
+
+    $mensagem = new Mensagem();
+
+    $mensagem->__set('para', $_POST['para']);
+    $mensagem->__set('assunto', $_POST['assunto']);
+    $mensagem->__set('mensagem', $_POST['mensagem']);
+
+    //print_r($mensagem);
+
+    if($mensagem->mensagemValida()) {
+        echo 'A mensagem é valida';
+    } else {
+        echo 'A mensagem não é valida';
+    }
